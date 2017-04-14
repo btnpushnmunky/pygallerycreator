ba0ef5e203e2bac593cf756b73afefd747f74b2e
Dan Ross <dan@rosspixelworks.com>
2017-04-13 20:20:57 -0500

    Beginning work on gallery_creator

---
+++ tests/test_gallery_creator.py
@@ -0,0 +1,13 @@
+def test_create_image_list():
+    image_list = None
+    assert image_list
+
+
+def test_found_template():
+    template = None
+    assert template
+
+
+def test_got_image_dir():
+    dir = None
+    assert dir
