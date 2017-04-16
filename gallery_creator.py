3fe1b7222e730a136b609377a74fcc884a610750
Dan Ross <dan@rosspixelworks.com>
2017-04-15 23:32:21 -0500

    Successfully using Jinja2 to render html

--- gallery_creator.py
+++ gallery_creator.py
@@ -1,17 +1,14 @@
-import bs4
 import os
-import logging
 from jinja2 import Environment, FileSystemLoader
 
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+JINJA_ENV = Environment(loader=FileSystemLoader("templates"))
 
 def get_image_list(dir_path):
     image_list = [image for image in os.listdir(dir_path)]
-    logger.debug(image_list)
     return image_list
 
-def create_html_file():
-    soup = "<!DOCTYPE html>"
-    return soup
+def create_html_file(image_dir):
+    template = JINJA_ENV.get_template("index.html")
+    images = get_image_list("tests/test_imgs")
+    return template.render(images=images)
--- tests/test_gallery_creator.py
+++ tests/test_gallery_creator.py
@@ -1,18 +1,23 @@
 import gallery_creator
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 def test_create_image_list():
     dir = os.path.dirname(__file__)
     test_img_path = os.path.join(dir, "test_imgs")
     image_list = gallery_creator.get_image_list(test_img_path)
+    logger.debug(image_list)
     assert len(image_list) == 3
-
 
 def test_got_image_dir():
     dir = None
     assert dir
 
-def test_read_template():
+def test_create_template():
     template = gallery_creator.create_html_file()
-    assert "<!DOCTYPE html>" in str(template)
+    logger.debug(template)
+    assert "<!DOCTYPE html>" in template
