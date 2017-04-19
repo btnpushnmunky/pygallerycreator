f0f65dc69380259050a429532df598674b54eab8
Dan Ross <dan@rosspixelworks.com>
2017-04-18 20:54:03 -0500

    Cleanup

--- tests/test_copier.py
+++ tests/test_copier.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
-from pathlib import Path
 from src import copier
+
 
 def test_index_template_exists():
     assert os.path.isfile("./templates/index.html")
@@ -19,6 +19,5 @@ def test_dist_creation():
     d = copier.get_desktop()
     temp_dir = tempfile.TemporaryDirectory(dir=d)
     temp_dir_path = os.path.join(d, temp_dir.name)
-    #copier.copy_template(temp_dir_path)
     copier.copy_resources(temp_dir_path)
     assert os.path.isdir(temp_dir_path)
