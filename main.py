42beaa4c850617229bfb872567fa4e88d85c4a1c
Dan Ross <dan@rosspixelworks.com>
2017-04-22 10:11:00 -0500

    Fixed messy import

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import src.copier import default_dist_path
+import src.copier
 import src.gallery_creator as gc
 import os
 
 
 def create_image_gallery(path=None):
     if path is None:
-        path = default_dist_path
+        path = src.copier.default_dist_path
     else:
         pass
     image_dir = input("Please enter the path to your image directory")
