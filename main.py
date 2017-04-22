7344d0220f2ac129f7a86bf33886eec3ff2f6abc
Dan Ross <dan@rosspixelworks.com>
2017-04-22 13:17:01 -0500

    Main testing

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import src.gallery_creator as gc
 import os
 
 
-def create_image_gallery(path=None):
-    if path is None:
-        path = src.copier.default_dist_path
+def get_user_path():
+    user_path = input("Please enter a destination directory for the gallery. Default is Desktop")
+    if user_path is None:
+        user_path = src.copier.default_dist_path
     else:
         pass
+    return user_path
+
+
+def create_image_gallery():
+    path = get_user_path()
     image_dir = input("Please enter the path to your image directory")
     html = gc.create_html(image_dir)
     src.copier.make_dist_dir(path)
---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+import main
+import logging
+from src.copier import default_dist_path
+
+
+def test_get_user_path(monkeypatch):
+    monkeypatch.setitem(__builtins__, "input", lambda x: "user dest dir")
+    user_path = main.get_user_path()
+    assert user_path == "user dest dir"
+
+
+def test_get_user_path_no_input(monkeypatch):
+    monkeypatch.setitem(__builtins__, "input", lambda x: None)
+    user_path = main.get_user_path()
+    assert user_path == default_dist_path
+
+
+def teardown_method():
+    main.input = input
