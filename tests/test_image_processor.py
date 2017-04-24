ec73e7168c26671ca039e8f188b23fdd5d0cc650
Dan Ross <dan@rosspixelworks.com>
2017-04-23 21:26:30 -0500

    Added initial tests for image_processor

---
+++ tests/test_image_processor.py
@@ -0,0 +1,21 @@
+import src.image_processor
+
+
+def test_image_creation():
+    assert False
+
+
+def test_thumb_dir_creation():
+    assert False
+
+
+def test_resized_img_dir_creation():
+    assert False
+
+
+def test_image_resize_and_save():
+    assert False
+
+
+def test_thumbnail_creation():
+    assert False
