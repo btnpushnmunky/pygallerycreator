7bc5402e1385dfda4167bbb061d2308e03adf46f
Dan Ross <dan@rosspixelworks.com>
2017-04-28 22:07:22 -0500

    Added requirements.txt

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,5 +1,10 @@
 from PIL import Image
 import os
 
+
 def create_thumb_dir(base_path):
    os.mkdir(os.path.join(base_path,"thumbs"))
+
+
+def create_resized_images_dir(base_path):
+   os.mkdir(os.path.join(base_path, "large_imgs"))
