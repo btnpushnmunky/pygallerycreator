32374cc8eb0610ad8df306c6453119c30ab521d1
Dan Ross <dan@rosspixelworks.com>
2017-04-28 21:23:29 -0500

    Thumb dir creation tests pass

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,2 +1,5 @@
 from PIL import Image
+import os
 
+def create_thumb_dir(base_path):
+   os.mkdir(os.path.join(base_path,"thumbs"))
--- tests/test_image_processor.py
+++ tests/test_image_processor.py
@@ -1,12 +1,16 @@
 import src.image_processor
-
+import tempfile
+import os
 
 def test_image_creation():
     assert False
 
 
 def test_thumb_dir_creation():
-    assert False
+    test_dir = tempfile.TemporaryDirectory()
+    test_dir_path = os.path.join(tempfile.gettempdir(), test_dir.name)
+    src.image_processor.create_thumb_dir(test_dir_path)
+    assert os.path.isdir(os.path.join(test_dir_path, "thumbs"))
 
 
 def test_resized_img_dir_creation():
