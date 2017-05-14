3569b1e4d3ff289bcf69868cb81597b8766ba114
Dan Ross <dan@rosspixelworks.com>
2017-05-13 23:46:03 -0500

    Change to main.py so that the dist_dir is created before trying to write to it. Duh.

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def create_image_gallery():
     user_path = get_user_path()
     image_dir = input("Please enter the path to your image directory")
     html = gc.create_html(image_dir)
-    src.image_processor.create_images(image_dir)
     src.copier.make_dist_dir(user_path)
     src.copier.copy_resources(user_path)
+    src.image_processor.create_images(image_dir, user_path)
     with open(os.path.join(user_path, "index.html"), "w+") as f:
         f.write(html)
 
