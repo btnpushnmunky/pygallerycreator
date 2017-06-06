5050b141d0a830e3d06adf7bdcf54de2450e84a4
Dan Ross <dan@rosspixelworks.com>
2017-06-05 21:04:51 -0500

    Flake8 changes

--- pygallerycreator/gallery_creator.py
+++ pygallerycreator/gallery_creator.py
@@ -7,7 +7,8 @@ JINJA_ENV = Environment(loader=FileSystemLoader("templates"))
 
 def get_image_list(dir_path):
     """
-    Get the image list
+    Get the image list.
+
     :param dir_path: Path to the directory of images
     :return:  A list of image filenames
     """
@@ -17,7 +18,8 @@ def get_image_list(dir_path):
 
 def create_html(image_dir):
     """
-    Create the html for the template
+    Create the html for the template.
+
     :param image_dir: Path to the directory of images
     :return: HTML for the index.html file
     """
