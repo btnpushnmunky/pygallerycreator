b2cb8b536c19a2bd0b74e432174c74e435f4c331
Dan Ross <dan@rosspixelworks.com>
2017-06-07 18:09:39 -0500

    Removed f strings

--- pygallerycreator/test_gallery_creator.py
+++ pygallerycreator/test_gallery_creator.py
@@ -10,13 +10,15 @@ TEST_IMG_DIR = os.path.join(test_file_dir, "test_imgs")
 
 
 def test_create_image_list():
+    """Test creating the image list."""
     image_list = gallery_creator.get_image_list(TEST_IMG_DIR)
-    logger.debug(f"Test list is {image_list}")
+    logger.debug("Test list is {0}".format(image_list))
     assert len(image_list) == 3
 
 
 def test_create_html():
+    """Test creating the gallery."""
     html = gallery_creator.create_html(TEST_IMG_DIR)
     html_length = len(html)
-    logger.debug(f"HTML length {html_length}")
+    logger.debug("HTML length {0}".format(html_length))
     assert "<!DOCTYPE html>" in html
