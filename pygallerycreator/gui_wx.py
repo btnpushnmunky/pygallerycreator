9a3b2df14e6c4030b55c812533e5235750013507
Dan Ross <dan@rosspixelworks.com>
2017-06-18 19:40:07 -0500

    Beginning wx gui

---
+++ pygallerycreator/gui_wx.py
@@ -0,0 +1,17 @@
+import wx
+import main
+
+
+class GalleryApp(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(400,200))
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        self.button = wx.Button(self, -1, "Create Gallery")
+        hbox.Add(self.button, border=50)
+        self.SetSizer(hbox)
+        self.Show(True)
+
+app = wx.App(False)
+frame = GalleryApp(None, 'PyGalleryCreator')
+app.MainLoop()
